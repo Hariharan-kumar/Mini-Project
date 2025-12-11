d77ca3dc7486153a7304f8598714c6cae44e6ee4
hari-test-2001 <hariharank2018@gmail.com>
2025-12-11 18:17:21 +0530

    Deleted

--- ECommerce Microservices Project/ProductService/ProductService/src/main/java/com/ecommerce/product_service/dao/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.product_service.dao;
-
-import com.ecommerce.product_service.entity.ProductEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepo extends MongoRepository<ProductEntity,Long> {
-    ProductEntity findTopByOrderByIdDesc();
-}
